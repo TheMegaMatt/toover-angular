4cde84746b9ecd5e92bdd0fe5d4e790e30682547
Matteo Murdocco <matteo.murdocco@gmail.com>
2024-02-17 18:27:51 +0100

    Revert "feat: added login pages"
    This reverts commit c6b42b0f3ed468224bec4a59b210c0fc98a01a08.

--- src/core/components/base.component.ts
+++ src/core/components/base.component.ts
@@ -3,6 +3,7 @@ import {TranslateService} from "@ngx-translate/core";
 
 export class BaseComponent {
     protected translateService = inject(TranslateService);
+
     t(key: string, params: Record<string, string | number> = {} ) {
         return this.translateService.instant(key, params);
     }
--- src/features/auth/pages/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from "@angular/router";
-export const routes: Routes = [
-  {
-    path: "login",
-    loadComponent: () => import("./login/login.page").then((m) => m.LoginPage),
-  },
-  { path: "", redirectTo: "login", pathMatch: "full" },
-];
