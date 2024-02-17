f4380ca8cd52107b90f0bf68ac88ba4d7cc64e0a
Matteo Murdocco <matteo.murdocco@gmail.com>
2024-02-17 18:27:51 +0100

    Revert "feat: added dashboard layout + some basic pages"
    This reverts commit f1693687a5558d3b22f405e933bbf20b396aafc4.

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -2,12 +2,8 @@ import { Routes } from '@angular/router';
 import {AuthLayoutComponent as AuthShell} from "@shared/layouts/auth-layout/auth-layout.component";
 
 import {routes as auth} from '@/features/auth/pages/auth.routes'
-import {routes as dashboard} from '@/features/dashboard/pages/dashboard.routes'
-
-import {AppLayoutComponent} from "@shared/layouts/app-layout/app-layout.component";
 
 export const routes: Routes = [
-    { path: 'app', component: AppLayoutComponent, children: [...dashboard] },
     { path: 'auth', component: AuthShell, children: [...auth] },
-    { path: '',   redirectTo: '/app', pathMatch: 'full' },
+    { path: '',   redirectTo: '/auth', pathMatch: 'full' }
 ];
