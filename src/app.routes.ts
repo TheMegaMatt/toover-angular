f831e897e387fbf332c71ba78d1e5ee3a95949a0
Matteo Murdocco <TheMegaMatt@users.noreply.github.com>
2024-02-17 18:26:05 +0100

    feat: added dashboard layout + some basic pages

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 import {AuthLayoutComponent as AuthShell} from "@shared/layouts/auth-layout/auth-layout.component";
 
 import {routes as auth} from '@/features/auth/pages/auth.routes'
+import {routes as dashboard} from '@/features/dashboard/pages/dashboard.routes'
+
+import {AppLayoutComponent} from "@shared/layouts/app-layout/app-layout.component";
 
 export const routes: Routes = [
+    { path: 'app', component: AppLayoutComponent, children: [...dashboard] },
     { path: 'auth', component: AuthShell, children: [...auth] },
-    { path: '',   redirectTo: '/auth', pathMatch: 'full' }
+    { path: '',   redirectTo: '/app', pathMatch: 'full' },
 ];
---
+++ src/features/dashboard/pages/dashboard.routes.ts
@@ -0,0 +1,16 @@
+import {Routes} from "@angular/router";
+
+export const routes: Routes = [
+    {
+        path: "about",
+        loadComponent: () => import("./about/about.page").then((m) => m.AboutPage),
+    },
+    {
+        path: "settings",
+        loadComponent: () => import("./settings/settings.page").then((m) => m.SettingsPage),
+    },
+    {
+        path: "",
+        loadComponent: () => import("./home/home.page").then((m) => m.HomePage),
+    },
+];
