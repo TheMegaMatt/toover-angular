a0b706856db1e60c4eeab283829c8f7ee7ac4239
Matteo Murdocco <matteo.murdocco@gmail.com>
2024-02-17 18:27:51 +0100

    Revert "feat: added login pages"
    This reverts commit c6b42b0f3ed468224bec4a59b210c0fc98a01a08.

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,27 +1,19 @@
-import {ApplicationConfig, importProvidersFrom, InjectionToken} from "@angular/core";
-import {provideRouter} from "@angular/router";
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import {routes} from "./app.routes";
+import {routes} from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
 import {TranslateModule} from "@ngx-translate/core";
 import {provideTranslation} from "./app.i18n";
-import {getAuth} from 'firebase/auth';
-import {initializeApp} from "firebase/app";
-import {environment} from "./environments";
 
-const app = initializeApp(environment.firebase);
-
-export const AUTH = new InjectionToken("Firebase auth", {
-  providedIn: "root",
-  factory: () => {
-    return getAuth();
-  },
-});
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(),
-    importProvidersFrom(TranslateModule.forRoot(provideTranslation())),
-  ],
+    providers: [
+        provideRouter(routes),
+        provideHttpClient(),
+        importProvidersFrom(
+            TranslateModule.forRoot(provideTranslation())
+        )
+    ]
 };
+
--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import {AuthLayoutComponent as AuthShell} from "@shared/layouts/auth-layout/auth-layout.component";
 
-import {routes as auth} from '@/features/auth/pages/auth.routes'
-
-export const routes: Routes = [
-    { path: 'auth', component: AuthShell, children: [...auth] },
-    { path: '',   redirectTo: '/auth', pathMatch: 'full' }
-];
+export const routes: Routes = [];
