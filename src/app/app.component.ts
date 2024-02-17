21b8dc346b3c469035e2790836ec3628ded2c801
Matteo Murdocco <TheMegaMatt@users.noreply.github.com>
2024-02-17 15:16:25 +0100

    feat: installed tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
+  template: `<router-outlet></router-outlet>`,
   styles: [],
 })
 export class AppComponent {
-  title = 'toover';
 }
