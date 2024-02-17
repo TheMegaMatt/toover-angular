c6b42b0f3ed468224bec4a59b210c0fc98a01a08
Matteo Murdocco <TheMegaMatt@users.noreply.github.com>
2024-02-17 17:30:52 +0100

    feat: added login pages

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {AuthLayoutComponent as AuthShell} from "@shared/layouts/auth-layout/auth-layout.component";
 
-export const routes: Routes = [];
+import {routes as auth} from '@/features/auth/pages/auth.routes'
+
+export const routes: Routes = [
+    { path: 'auth', component: AuthShell, children: [...auth] },
+    { path: '',   redirectTo: '/auth', pathMatch: 'full' }
+];
