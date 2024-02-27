826c856386c76b0b516b3c6129fba7d25ed63ab4
Matteo Murdocco <m.murdocco@be-tse.it>
2024-02-27 12:37:45 +0100

    v1 app

---
+++ src/features/employees/components/employee-picker/employee-picker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oa-employee-picker',
+  standalone: true,
+  imports: [],
+  templateUrl: './employee-picker.component.html',
+  styles: ``
+})
+export class EmployeePickerComponent {
+
+}
---
+++ src/features/places/components/place-picker/place-picker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oa-place-picker',
+  standalone: true,
+  imports: [],
+  templateUrl: './place-picker.component.html',
+  styles: ``
+})
+export class PlacePickerComponent {
+
+}
