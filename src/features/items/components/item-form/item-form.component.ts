868625abf3024a43f03f131707f9a02c545e3665
Matteo Murdocco <m.murdocco@be-tse.it>
2024-02-27 12:37:45 +0100

    v1 app

---
+++ src/features/items/components/item-form/item-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oa-item-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-form.component.html',
+  styles: ``
+})
+export class ItemFormComponent {
+
+}
---
+++ src/features/types/components/type-picker/type-picker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oa-type-picker',
+  standalone: true,
+  imports: [],
+  templateUrl: './type-picker.component.html',
+  styles: ``
+})
+export class TypePickerComponent {
+
+}
